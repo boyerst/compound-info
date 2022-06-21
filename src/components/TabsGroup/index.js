7d7de00099e69bbf45c25b475b25b15fd2fe857c
Steve Boyer <stephen.a.boyer@gmail.com>
2022-06-21 17:56:59 -0500

    Add Compound logo next to Tabs

--- src/components/TabsGroup/index.js
+++ src/components/TabsGroup/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Image } from '@chakra-ui/react'
+import Logo from '../../assets/compound.png';
 import Compound from '../../views/Compound'
 import Tokens from '../../views/Tokens'
 
@@ -9,6 +10,7 @@ const TabsGroup = () => {
   return (
     <Tabs mt={30}>
       <TabList>
+        <Image src={Logo} htmlHeight='50' htmlWidth='50' mx={3} mb={2} mr={4} pointerEvents="none" />
         <Tab>One</Tab>
         <Tab>Two</Tab>
         <Tab>Three</Tab>
