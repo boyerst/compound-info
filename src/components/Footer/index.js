8c0d964d94af150953f2fbb5e357fe4a48e24232
Steve Boyer <stephen.a.boyer@gmail.com>
2022-06-23 14:27:02 -0500

    Add Compound social links to Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import {
-  Box
+  Box, 
+  Container,
+  Button, 
+  HStack
 } from '@chakra-ui/react'
+import { FaTwitter, FaDiscord } from 'react-icons/fa';
 
 
 
@@ -9,7 +13,21 @@ import {
 const Footer = () => {
 
   return (
-    <Box>FOOTER</Box>
+    <Box>
+      <Container>
+        <HStack>
+          <Button  leftIcon={<FaDiscord />} rounded={'full'} size='xs'>
+            Discord
+          </Button>
+          <Button colorScheme='twitter' leftIcon={<FaTwitter />} rounded={'full'} size='xs'>
+            Twitter
+          </Button>
+         </HStack>
+       
+      </Container>
+      
+    </Box>
+
   )   
 }
 
