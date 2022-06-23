774fa278f6bd0dcf48263db1d02a806183c6a24a
Steve Boyer <stephen.a.boyer@gmail.com>
2022-06-23 13:08:53 -0500

    Add working logic to add Token icons into Symbol column

--- src/views/Tokens/index.js
+++ src/views/Tokens/index.js
@@ -9,13 +9,15 @@ import {
   Td,
   TableCaption,
   TableContainer,
-  Tooltip
+  Tooltip,
+  Image
 } from '@chakra-ui/react'
 import { InfoOutlineIcon } from '@chakra-ui/icons'
 import { useQuery } from "@apollo/client"
 import { TOKENS_DATA } from '../../apollo/queries.js'
 import { formatNum } from '../../utils'
 import { formatPercentage } from '../../utils'
+
 
 
 
@@ -29,8 +31,6 @@ const Tokens = () => {
 
   console.log(tokensData)
 
-
-
   useEffect(() => {
     tokensPolling(1000)
   })
@@ -39,6 +39,21 @@ const Tokens = () => {
   function updateMe() {
     console.log("Queried TOKENS_DATA")
   }
+
+  // Working logic to insert Token icons into Symbol column
+  function importAll(r) {
+    let images = {}
+    r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); })
+    return images
+  }
+  const images = importAll(require.context('../../assets', false, /\.(png|jpe?g|svg)$/));
+  console.log(images)
+  const AaveKey = images[Object.keys(images)[0]]
+  const AaveName = Object.keys(images)[0]
+  console.log(AaveKey)
+  console.log(AaveName)
+
+
 
   return (
     <TableContainer mt={15}>
@@ -49,15 +64,15 @@ const Tokens = () => {
             <Th>Asset</Th>
             <Th>Symbol</Th>
             <Th>Price</Th>
-            <Th>TVL (ETH)</Th>
+            <Th>TVL (Ξ)</Th>
             <Th>cToken</Th>
             <Th textAlign='center'>Supply Rate (Annual)</Th>
             <Th textAlign='center'>Borrow Rate (Annual)</Th>
-            <Th>Total Borrows</Th>
+            <Th>Total Borrowed</Th>
             <Th>
               Borrow Index
               <Tooltip hasArrow placement='auto' closeDelay={500} label='The history of the markets borrow index return (Think S&P 500)'>
-                <InfoOutlineIcon ml={1}/>
+                <InfoOutlineIcon ml={1} mb={1}/>
               </Tooltip>
             </Th>
 
@@ -68,8 +83,11 @@ const Tokens = () => {
           : tokensData.markets.map(({ id, cash, name, symbol, underlyingSymbol, underlyingName, underlyingPriceUSD, supplyRate, borrowRate, totalBorrows, borrowIndex } : tokensData_markets) => (
           <Tbody  key={id}>
             <Tr>
-              <Td>{underlyingName}</Td>
-              <Td>{underlyingSymbol}</Td>
+              <Td fontWeight='extrabold'>{underlyingName}</Td>
+              <Td sx={{display: 'flex'}}>
+                <Image src={AaveKey} mr={1} htmlHeight='15' htmlWidth='15'/>
+                {underlyingSymbol}
+              </Td>
               <Td>$ {formatNum(underlyingPriceUSD)}</Td>
               <Td>{formatNum(cash)}</Td>
               <Td>{symbol}</Td>
