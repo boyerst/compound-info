c7565b82c9a0adaef6f8a2fbc4e390a40cb2ef8a
Steve Boyer <stephen.a.boyer@gmail.com>
2023-05-26 12:53:29 -0400

    Adjust margin-top of landing logo

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -17,7 +17,7 @@ const Landing = props => {
   console.log(props)
 
   return (
-    <VStack spacing={14} mt={150}>
+    <VStack spacing={14} mt={235}>
       <Logo mb={10} h="40vmin" pointerEvents="none" />
       <Text fontSize="xl" fontWeight="extrabold">
         compound.info
